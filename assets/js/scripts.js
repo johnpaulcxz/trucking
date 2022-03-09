e91d2a25c19cf0e31f85ff2bba1c362e20800b52
johnpaulcxz <johnpaulmarquina@gmail.com>
2022-03-09 16:16:13 +0800

    changes

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
     $(".fancybox-link").fancybox({
-        padding: 0,
+        // padding: 0,
         helpers: {
             overlay: {
                 locked: false
